6a4a72f1b5b712a3ec9ef3a27cf599891a8beac4
Ali <ali@pop-os.localdomain>
2022-01-09 13:54:42 -0500

    phase one done

--- index.js
+++ index.js
@@ -6,15 +6,126 @@ const fetchData = async (searchTerm) => {
         }
     }
     const response = await axios.get('http://www.omdbapi.com/', config);
-    console.log(response.data);
+    if (response.data.Error) {
+        return [];
+    };
+    return response.data.Search
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b> Search for a movie</b></label>
+<input class= "input" />
+<div class="dropdown">
+<div class= "dropdown-menu">
+<div class= "dropdown-content results"></div>
+</div>
+</div>
+`;
+
+
+ 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 
 
 
-const onInput =event => {
-    fetchData(event.target.value);
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+
+    if(!movies.length){
+        dropdown.classList.remove('is-active');
+        return;
+    }
+    resultsWrapper.innerHTML='';
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+        const option = document.createElement('a');
+
+        const imgSrc = movie.Poster ==='N/A' ? '' : movie.Poster;
+
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
+       <img src = "${imgSrc}" />
+        ${movie.Title}
+       `;
+
+       option.addEventListener('click',event =>{
+           dropdown.classList.remove('is-active');
+           input.value = movie.Title;
+           onMovieSelect(movie);
+
+       });
+
+        resultsWrapper.appendChild(option);
+    }
+
 };
-input.addEventListener('input', debounce(onInput,500));
+input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => { 
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active');
+    }
+});
+
+
+const onMovieSelect = async movie =>{
+
+    const config = {
+        params: {
+            apikey: '2dfc1309',
+            i: movie.imdbID
+        }
+    }
+ 
+ const response = await axios.get('http://www.omdbapi.com/', config);
+ 
+document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+ 
+};
+ 
+
+const movieTemplate = (movieDetail) =>{
+return `
+
+<article class= media>
+<figure class = "media-left">
+<p class ="image">
+<img src="${movieDetail.Poster}" />
+</p>
+</figure>
+<div class= "media-content">
+<div class= "content">
+<h1>${movieDetail.Title}</h1>
+<h4>${movieDetail.Genre}</h4>
+<p>${movieDetail.Plot}</p>
+</div>
+</div>
+</article>
+
+<article class= "notification is-primary">
+<p class = "title">${movieDetail.BoxOffice}</p>
+<p class="subtitle">Box Office</p>
+</article>
+
+<article class= "notification is-primary">
+<p class = "title">${movieDetail.Metascore}</p>
+<p class="subtitle">MetaScore</p>
+</article>
+
+<article class= "notification is-primary">
+<p class = "title">${movieDetail.imdbRating}</p>
+<p class="subtitle">IMDB rating</p>
+</article>
+
+<article class= "notification is-primary">
+<p class = "title">${movieDetail.imdbVotes}</p>
+<p class="subtitle">IMDB Votes</p>
+</article>
+
+
+`;
+};
