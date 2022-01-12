1d4cb0afa293b73918366df42060b0346094ae7a
Ali <ali@pop-os.localdomain>
2022-01-11 19:02:26 -0500

    added logic to attributes with same results

--- index.js
+++ index.js
@@ -92,13 +92,21 @@ const runComparison = () =>{
 
 
 
-        }else{
+        }else if(rightSideValue < leftSideValue){
             rightStat.classList.remove('is-primary');
             rightStat.classList.add('is-danger');
             
             
 
 
+        }else if(rightSideValue === leftSideValue){ 
+            rightStat.classList.remove('is-primary');
+            leftStat.classList.remove('is-primary');
+            rightStat.classList.remove('is-danger');
+            leftStat.classList.remove('is-danger');
+            
+            rightStat.classList.add('is-warning');
+            leftStat.classList.add('is-warning');
         }
 
 
